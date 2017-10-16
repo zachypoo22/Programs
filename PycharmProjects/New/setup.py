e28b619bb45b2182f6a2012a3b351daa3462d6bc
zachypoo22 <zachm93@live/com>
2017-10-16 17:53:28 -0400

    finished real project1

---
+++ PycharmProjects/New/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='project1',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='zachm',
+    author_email='',
+    description=''
+)
