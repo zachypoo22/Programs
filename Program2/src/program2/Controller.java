6b2953a5824911f270d85cb95c85e2afa580ad63
zacheriah <zacheriah@localhost>
2017-10-19 16:18:40 -0400

    fixed some more program 2

--- Program2/src/program2/Controller.java
+++ Program2/src/program2/Controller.java
@@ -4,6 +4,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -11,6 +13,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -30,6 +33,7 @@ public class Controller
     private boolean fileLoaded = false;
     private boolean fileExists = false;
     private boolean fileModified = false;
+    private boolean subdir = false;
 
     //filename check
     public static String correctedName(String s)
@@ -60,6 +64,7 @@ public class Controller
         frame.setTitle("Text Editor");
         frame.setLocationRelativeTo(null);
         frame.textArea().enable(false);
+        frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 
 //        //filename tests
 //        System.out.println("text => " + correctedName("text"));
@@ -102,7 +107,9 @@ public class Controller
                         return;
                     }
                 }
-
+                
+                fileExists = false;
+                frame.textArea().setText("");
                 frame.textArea().enable(true);
                 frame.label().setText("*");
                 fileLoaded = true;
@@ -180,8 +187,9 @@ public class Controller
                         return;
                     }
                 }
-                File file = new File("" + working + "/" + correctedName(file1.getName()));
-                Path path = file.toPath();
+//                File file = new File("" + working + "/" + correctedName(file1.getName()));
+
+                Path path = file1.toPath();
 
                 try
                 {
@@ -193,6 +201,10 @@ public class Controller
 
                     frame.textField().setText("" + relative);
 
+                    String fname = file1.getName();
+//                    file1.renameTo(new File(correctedName(fname)));
+                    Files.move(file1.toPath(), file1.toPath().resolveSibling(correctedName(fname)));
+
                 }
                 catch (IOException e)
                 {
@@ -201,6 +213,7 @@ public class Controller
 
                 frame.label().setText("");
                 fileModified = false;
+                fileExists = true;
             }
         });
         frame.saveItem().addActionListener(new ActionListener()
@@ -212,14 +225,13 @@ public class Controller
                 try
                 {
                     Path working = Paths.get(System.getProperty("user.dir"));
-
-                    File file = new File(working.toString(), frame.textField().getText());
-                    System.out.println(frame.textField().getText());
-
+                    String workingstr = working.toString();
+//                    if(subdir)
+//                    {
+//                        workingstr = workingstr + "/subdir/";
+//                    }
+                    File file = new File(workingstr, frame.textField().getText());
                     Path path = file.toPath();
-
-                    Path relative = working.relativize(path);
-
                     String content = frame.textArea().getText();
 
                     Files.deleteIfExists(path);
@@ -270,6 +282,66 @@ public class Controller
             public void menuCanceled(MenuEvent me)
             {
 
+            }
+        });
+
+        frame.addWindowListener(new WindowListener()
+        {
+            @Override
+            public void windowOpened(WindowEvent we)
+            {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void windowClosing(WindowEvent we)
+            {
+                if (fileModified)
+                {
+                    int reply = JOptionPane.showConfirmDialog(frame, "Do you want to discard your changes?");
+                    if (reply == JOptionPane.YES_OPTION)
+                    {
+                        System.exit(0);
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else
+                {
+                    System.exit(0);
+                }
+            }
+
+            @Override
+            public void windowClosed(WindowEvent we)
+            {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void windowIconified(WindowEvent we)
+            {
+                //   throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent we)
+            {
+                // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void windowActivated(WindowEvent we)
+            {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent we)
+            {
+                //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
             }
         });
 
