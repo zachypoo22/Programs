99c0b7e6672e7d17db048e24c75ecb9ce1c27cc7
zachypoo22 <zachm93@live/com>
2017-11-16 08:45:20 -0500

    added program 3

---
+++ program3/src/program3/Controller.java
@@ -0,0 +1,19 @@
+
+package program3;
+
+/**
+ *
+ * @author Zacheriah Mell
+ */
+public class Controller 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+    }
+
+}
