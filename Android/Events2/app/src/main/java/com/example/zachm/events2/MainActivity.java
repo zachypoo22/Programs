9819235a08cf0ec9e95f8d6130c0757248a1c7a9
zachypoo22 <zachm93@live/com>
2018-01-18 17:30:39 -0500

    got that event goin'

--- Android/Events2/app/src/main/java/com/example/zachm/events2/MainActivity.java
+++ Android/Events2/app/src/main/java/com/example/zachm/events2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.zachm.events2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goButton = (Button) findViewById(R.id.goButton);
+        goButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v) {
+                        TextView textView = (TextView) findViewById(R.id.textField);
+                        textView.setText("Good job!");
+                    }
+                }
+        );
     }
 }
