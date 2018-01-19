a90e9231cbb0e2d5254289a4c8c38341d3f316e0
zachypoo22 <zachm93@live/com>
2018-01-19 00:32:51 -0500

    finished gestures project

--- Android/GesturesDemo/app/src/main/java/com/example/zachm/gesturesdemo/MainActivity.java
+++ Android/GesturesDemo/app/src/main/java/com/example/zachm/gesturesdemo/MainActivity.java
@@ -1,13 +1,83 @@
 package com.example.zachm.gesturesdemo;
 
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.GestureDetector.OnDoubleTapListener;
+import android.view.MotionEvent;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener, OnDoubleTapListener{
+
+    private TextView textField;
+    private GestureDetectorCompat gestureDetectorCompat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textField = (TextView) findViewById(R.id.textField);
+        this.gestureDetectorCompat = new GestureDetectorCompat(this, this);
+        gestureDetectorCompat.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        textField.setText("single tap confirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        textField.setText("double tap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        textField.setText("double tap event");
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        textField.setText("down");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+        textField.setText("Show Press");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        textField.setText("single tap up");
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        textField.setText("scroll");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+        textField.setText("long press");
+    }
+
+    @Override
+    public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        textField.setText("fling");
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gestureDetectorCompat.onTouchEvent(event);
+        return super.onTouchEvent(event);
     }
 }
